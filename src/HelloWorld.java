7256d7d4ae6275d41e95b29aed4c4b4b397c8c3f
zhangyunfei <245124085@qq.com>
2021-01-10 22:23:34 +0800

    2020.1.10

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+1.第一行的第三个单词必须和所在的文件名称完全一致
+2.public class后面代表定义一个类的名称，类是Java当中所有源代码的基本组织单位
+ */
+public class HelloWorld {
+    //main方法，程序从这里执行
+    public static void main(String[] args) {
+        //输出语句
+        System.out.println("Hello world");
+    }
+}
