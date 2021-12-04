ca94618ead50de6521c48dfeb4c7b8ce09b0cb5e
245124085@qq.com <245124085@qq.com>
2021-12-04 11:02:12 +0800

    Hello World 第一天

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,8 @@
 
 
-
-
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world 替一天 测试合并分支");
+        System.out.println("Hello world");
     }
 
 }
