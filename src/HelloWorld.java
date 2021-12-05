2c0733cc648194c762404e596b34478d0795719a
245124085@qq.com <245124085@qq.com>
2021-12-05 21:45:01 +0800

    2-3节

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,11 @@
 
-
+//第一行的第三个单词必须和所在的文件名称完全一样，大小写也要一样
+//public class后面代表定义一个类的名称，类是java当中所有源代码的基本组织单位
 public class HelloWorld {
+    //第二行的内容是万年不变的固定写法，代表main方法
+    //第一行代表程序执行的起点
     public static void main(String[] args) {
+        //第三行代表打印输出语句
         System.out.println("Hello world");
     }
 
---
+++ src/常量和变量.java
@@ -0,0 +1,29 @@
+public class 常量和变量 {
+
+
+    public static void main(String[] args) {
+        //字符串常量
+        System.out.println("abc");
+        System.out.println("");//可为空
+
+        //整数常量
+        System.out.println(123);
+
+        //浮点数常量
+        System.out.println(1.12);
+
+        //字符常量
+        System.out.println('a');
+//        System.out.println('');//单引号中必须有内容
+//        System.out.println('ab');//只能有一个字符
+
+        //布尔常量
+        System.out.println(true);
+        System.out.println(false);
+
+        //空常量-不能直接用来打印输出
+//        System.out.println(null);
+    }
+
+
+}
